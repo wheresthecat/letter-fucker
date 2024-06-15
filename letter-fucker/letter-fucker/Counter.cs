816d6c35adf801734f35e6f5ed874e5845e5527a
wheresthecat <frosty@centrum.cz>
2024-06-16 00:37:51 +0200

    Update Counter.cs

--- letter-fucker/letter-fucker/Counter.cs
+++ letter-fucker/letter-fucker/Counter.cs
@@ -1,6 +1,11 @@
 ﻿namespace letter_fucker;
 
-public class Counter
+public static class Counter
 {
-    
+    public static List<Dictionary<char, int>> Count(string textInput)
+    {
+        List<Dictionary<char, int>> output;
+
+        return output;
+    }
 }
