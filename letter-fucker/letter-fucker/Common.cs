1c367402556f8b2b1b794275e1cc9bac12418e4d
wheresthecat <frosty@centrum.cz>
2024-06-16 22:18:40 +0200

    Create Common.cs

---
+++ letter-fucker/letter-fucker/Common.cs
@@ -0,0 +1,13 @@
+﻿namespace letter_fucker;
+
+public static class Common
+{
+    public static double GetPercents(int part, int whole)
+    {
+        double output = 0.00f;
+
+        output = part / (whole / 100);
+
+        return output;
+    }
+}
