a3b670ab34ed116b3d5c3dfda69c02dac0115fbc
wheresthecat <frosty@centrum.cz>
2024-06-16 17:24:47 +0200

    Update Program.cs

--- letter-fucker/letter-fucker/Program.cs
+++ letter-fucker/letter-fucker/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.WriteLine("Let the letter-fucking begin! :D");
 
-        string testData = "AAAAaaaaBBBbbbQQ";
+        string testData = "I worked at a walmart as a cashier while in college for about 3 months just to get some cash for books. I never had to do anything really demeaning, I'm pretty sure the managers bullshitted a lot of things, but I do have a funny story. While working there one of the managers got fired, and his replacement learned I was a chemical engineering student. So he pulled me into his office and genuinely asked me how they make water. I thought he was joking but once I figured out he was being serious I tried to get him to elaborate to which he explained that he thought tap water came from the water factory. I kinda explained that water wasn't really made and it was filtered from a sewage treatment plant and he got pretty concerned. He asked if bottled water was safe and I just said sure to quell his worries and from that day on he was never seen drinking out of anything but bottled water. The day I quit he shook my hand and said thank you for explaining it to him. For all I know he's still out there sippin' a bottled water.";
 
         Dictionary<char, int> pairs = new Dictionary<char, int>();
         
@@ -17,9 +17,10 @@ class Program
                 UpdateDict(pairs, letter);
             }
         }
-        PrintSet(pairs);
-        
+        PrintSet(OrderByValue(pairs));
     }
+    
+    // Update or add new. If key is already existed it just increment the value by one. If not, adds a new key/value part.
 
     public static void UpdateDict(Dictionary<char, int> inputDict, char inputKey)
     {
@@ -47,6 +48,18 @@ class Program
             Console.WriteLine($"{item.Key} - {item.Value}");
         }
         Console.WriteLine("---");
+    }
+
+    public static Dictionary<char, int> OrderByValue(Dictionary<char, int> input)
+    {
+        Dictionary<char, int> orderedDict = new Dictionary<char, int>();
+
+        foreach (KeyValuePair<char, int> item in input.OrderByDescending(key => key.Value))
+        {
+            orderedDict.Add(item.Key, item.Value);
+        }
+
+        return orderedDict;
     }
 }
 
